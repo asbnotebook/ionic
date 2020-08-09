c2a7f5f161a41c571f32cbf217c551f6464e07a5
asbnotebook <asbnotebook@gmail.com>
2020-08-09 21:47:09 +0530

    To do App

---
+++ toDoApp/src/app/home/home.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { DbService, ToDoInterface } from '../services/db.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  todos: ToDoInterface[];
+  todoInput: string;
+
+  constructor(private dbService: DbService) { }
+
+  ionViewDidEnter() {
+    this.todoInput = "";
+    this.dbService.getAllToDos().then(data => this.todos = data);
+  }
+
+ addToDo(description: string) {
+    this.dbService.addToDo(description).then(data => {
+      this.todos = data;
+      this.todoInput = "";
+    });
+  }
+
+  deleteToDo(id: number) {
+    this.dbService.deleteToDo(id)
+      .then(data => this.todos = data);
+  }
+
+  doneToDo(id: number) {
+    this.dbService.updateToDo(id)
+      .then(data => this.todos = data);
+  }
+}
